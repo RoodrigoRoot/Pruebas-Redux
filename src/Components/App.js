bc4e118af67f9c32df36211201b1c5fc83b103bd
Rodrigo <leycourcino@gmail.com>
2019-07-10 16:58:33 -0500

    Commit

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React  from 'react'
+import Menu from './Menu'
+import Usuarios from "./Usuarios"
+import {BrowserRouter, Route} from 'react-router-dom'
+const Tareas = () => <div>Tareas</div>
+
+const App = ()=>(
+  <BrowserRouter>
+  <Menu/>
+    <div className="margen">
+    <Route exact path="/" component={Usuarios}/>
+    <Route exact path="/tareas" component={Tareas}/>
+    </div>
+  </BrowserRouter>
+
+)
+export default App
---
+++ src/Components/Usuarios/index.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+import {connect} from 'react-redux'
+
+
+class Usuarios extends Component {
+constructor(){
+  super()
+  this.state={
+    usuarios:[]
+  }
+}
+   ponerFilas = ()=> (
+     
+        this.state.usuarios.map( (usuario)=> (
+          
+          <tr key={usuario.id}>
+            <td>
+              {usuario.name}
+            </td>
+            <td>
+            {usuario.email}
+          </td>
+          <td>
+          {usuario.website}
+        </td>
+          </tr>
+          
+        
+        )  )
+
+     
+   )
+
+async componentDidMount(){
+  const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users/')
+  this.setState(   {
+    usuarios: respuesta.data
+
+  }  )
+}
+
+
+render(){
+
+  return( 
+    <div  >
+    <table className="tabla">
+      <thead>
+        <tr>
+          <th>
+            Nombre
+          </th>
+          <th>
+            Correo
+          </th>
+          <th>
+            Enlace
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+          {this.ponerFilas()}       
+      </tbody>
+    </table>
+    </div>
+  )
+}
+}
+export default  Usuarios
